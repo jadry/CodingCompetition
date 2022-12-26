1836734d2a10bd81580fac8cfde64a4831907f87
sylee <sylee@ict-lab.com>
2022-12-26 12:51:30 +0900

    2022년 12월 2번 문제

---
+++ USACO_202212_P2/CodingTest.cpp
@@ -0,0 +1,140 @@
+﻿// CodingTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/* P1
+int main()
+{
+	int n;
+
+	cin >> n;
+	vector<long long> c(n);
+	for (int i = 0; i < n; i++)
+		cin >> c[i];
+
+	sort(c.begin(), c.end());
+
+	long long max = 0;
+	long tuition = 0;
+
+	long long crnt = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		crnt = c[i] * (n - i);
+		if (crnt > max)
+		{
+			max = crnt;
+			tuition = c[i];
+		}
+	}
+
+	cout << max << " " << tuition << "\n";
+
+}
+*/
+
+
+
+void solve()
+{
+	int n, k;
+	string breed;
+	cin >> n >> k;
+	cin >> breed;
+
+	string patch(n, '.');
+	int count = 0;
+
+	for (int i = k; i < n ; i++)
+	{
+		if (i < n-1 && breed[i - k] == '.')
+			continue;
+
+		for (int j = i - k; j <= i+k; j++)
+		{
+			if (j >= n)
+				continue;
+
+			if (patch[i] == '.' && breed[j] != '.')// i 위치에 패치 놓을 조건 
+			{
+				 
+				patch[i] = breed[j];
+				count++;
+				breed[j] = '.';
+			}
+			else if (patch[i] != '.' && patch[i] == breed[j])// i위치의 패치와 같은 소를 feed 처리
+			{
+				breed[j] = '.';
+			}
+		}
+	}
+
+	int index = -1;
+	for (int i = n - 1; i >= n - k - 1; i--)
+	{
+		if (index == -1 && patch[i] == '.')
+			index = i;
+	}
+
+	if( index >= 0)
+	{
+		for (int i = n - 1; i >= n - k - 1; i--)
+		{
+			if (breed[i] != '.')
+			{
+				patch[index] = breed[i];
+				breed[i] = '.';
+				count++;
+				break;
+			}
+		}
+
+	}
+
+	cout << count << "\n";
+	for (char c : patch)
+	{
+		cout << c;
+	}
+	cout << "\n";
+}
+
+
+int main()
+{
+	// 공부하자!( 2022.12.19 )
+	//int a = 3000000;
+	//int n = 10000;
+
+	//long long c = a * n;
+
+	//c = 3000000 * 10000;
+
+	//c = 3000000LL * 10000LL;
+
+	//-----------------------------------------
+
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	int t;
+	
+	cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		solve();
+
+		//cout << n << " " << k << "\n";
+		//cout << breed << "\n";
+	}
+
+}
---
+++ USACO_202212_P2_2/CodingTest.cpp
@@ -0,0 +1,82 @@
+﻿// CodingTest.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int t;
+	int n, k;
+	cin >> t;
+	while (t--)
+	{
+		string cow;
+
+		cin >> n >> k;
+		cin >> cow;
+		string p(n, '.');
+
+		int nearestG = -k - 1;
+		int nearestH = -k - 1;
+		int count = 0;
+
+
+		for (int i = 0; i < n; i++)
+		{
+			if (cow[i] == 'G' && i - nearestG > k)
+			{
+				// G
+				if (i + k < n)
+				{
+					p[i + k] = 'G';
+					nearestG = i + k;
+					count++;
+				}
+				else if (p[i] == '.')
+				{
+					p[i] = 'G';
+					nearestG = i;
+					count++;
+				}
+				else
+				{
+					p[i-1] = 'G';
+					nearestG = i-1;
+					count++;
+				}
+
+			}
+			if (cow[i] == 'H' && i - nearestH > k)
+			{
+				// H
+				if (i + k < n)
+				{
+					p[i + k] = 'H';
+					nearestH = i + k;
+					count++;
+				}
+				else if (p[i] == '.')
+				{
+					p[i] = 'H';
+					nearestH = i;
+					count++;
+				}
+				else
+				{
+					p[i - 1] = 'H';
+					nearestH = i - 1;
+					count++;
+				}
+			}
+
+
+		}
+		cout << count << "\n" << p << "\n";
+
+
+	}
+
+
+}
